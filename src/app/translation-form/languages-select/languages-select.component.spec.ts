41e214ab8ded868591dd82f80f67f2e172e6a1e7
Ilya Kondratiev <ikondratev@lanit.ru>
2021-04-15 17:13:25 +0300

    ...

--- src/app/translation-form/languages-select/languages-select.component.spec.ts
+++ src/app/translation-form/languages-select/languages-select.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LanguagesSelectComponent } from './languages-select.component';
 
@@ -6,7 +6,7 @@ describe('LanguagesSelectComponent', () => {
   let component: LanguagesSelectComponent;
   let fixture: ComponentFixture<LanguagesSelectComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LanguagesSelectComponent ]
     })
--- src/app/translation/translation-detail/translation-detail.component.spec.ts
+++ src/app/translation/translation-detail/translation-detail.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TranslationDetailComponent } from './translation-detail.component';
 
@@ -6,7 +6,7 @@ describe('TranslationDetailComponent', () => {
   let component: TranslationDetailComponent;
   let fixture: ComponentFixture<TranslationDetailComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TranslationDetailComponent ]
     })
