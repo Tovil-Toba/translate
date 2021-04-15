d5a967748efbd43e330fb1cdc5f5741302ffa32d
Ilya Kondratiev <ikondratev@lanit.ru>
2021-04-15 16:48:01 +0300

    ...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { TranslationFormComponent } from './translation-form/translation-form.component'
+import { TranslationComponent } from './translation/translation.component'
+import { TranslationsComponent } from './translations/translations.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'translation-form', component: TranslationFormComponent },
+  { path: 'translation/:id', component: TranslationComponent },
+  { path: 'translations', component: TranslationsComponent },
+  { path: '',   redirectTo: '/translation-form', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,56 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
+import { MatCardModule } from '@angular/material/card'
+import { MatListModule } from '@angular/material/list'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { NgxIndexedDBModule } from 'ngx-indexed-db'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { TranslationsComponent } from './translations/translations.component'
+import { TranslationComponent } from './translation/translation.component'
+import { TranslationFormComponent } from './translation-form/translation-form.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+import { dbConfig } from './shared/db-config';
+import { TranslationDetailComponent } from './translation/translation-detail/translation-detail.component';
+import { LanguagesSelectComponent } from './translation-form/languages-select/languages-select.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TranslationsComponent,
+    TranslationComponent,
+    TranslationFormComponent,
+    PageNotFoundComponent,
+    TranslationDetailComponent,
+    LanguagesSelectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    MatListModule,
+    MatGridListModule,
+    NgxIndexedDBModule.forRoot(dbConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
